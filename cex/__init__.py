464e5697ce58bbc1bbf6f17c3c221fde68b97af2
Buddy ™️ by STRMKRD ™️ <advancedistroadmin@gmail.com>
2026-09-19 22:33:53 -0400

    Initialize CEX-001 research package

---
+++ cex/__init__.py
@@ -0,0 +1,4 @@
+"""CEX-001: deterministic budgeted adaptive compute research prototype."""
+from .engine import Action, Exchange, verify_receipts
+
+__all__ = ["Action", "Exchange", "verify_receipts"]
